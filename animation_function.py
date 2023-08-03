c658f6064a69bebefc3891b9f9a47500b61fffcf
Szymon Szewczyk <szewczyk.szy@gmail.com>
2023-08-03 11:42:01 +0200

    created documentation

--- animation_function.py
+++ animation_function.py
@@ -2,7 +2,18 @@ import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 
 
-def animate(i, file_name, f, a):
+def animate(i, file_name, fig, ax):
+    """
+    Function creating live updating graphs. Parameter "func" of the 
+    animation.FuncAnimation class. the function is called at each frame.  
+
+    Parameters:
+        i - Each frame of the animation.
+        file_name - Name of temporary file where sensor data are stored.
+        fig - Figure, The top level container for all the plot elements.
+        ax - An Axes object encapsulates all the elements of an individual subplot in a figure.
+
+    """
     pullData = open(file_name, "r").read()
     dataList = pullData.split('\n')
     xList = []
@@ -12,22 +23,19 @@ def animate(i, file_name, f, a):
             x, y = eachLine.split(',')
             xList.append(int(x))
             yList.append(int(y.strip()))
-    a[0,0].clear()
+    ax[0, 0].clear()
     if len(xList) == 0:
-        a[0,0].set_ylim([-1, 1])
+        a[0, 0].set_ylim([-1, 1])
     else:
-        a[0,0].set_ylim([-1, max(yList[-400:])+1])
+        a[0, 0].set_ylim([-1, max(yList[-400:])+1])
     if len(xList) >= 400:
-        a[0,0].plot(xList[-400:], yList[-400:])
+        a[0, 0].plot(xList[-400:], yList[-400:])
     else:
-        a[0,0].plot(xList, yList)
+        a[0, 0].plot(xList, yList)
 
-    ############ future ############
-    pullData = open(file_name, "r").read()
-    dataList = pullData.split('\n')
-    xList_1 = []
-    xList_2 = []
-    xList_3 = []
-    xList_4 = []
-    yList = []
-    
+    # TODO: extend plotting more charts, when more sensors will be connected
+    # xList_0_0 = []
+    # xList_0_1 = []
+    # xList_1_0 = []
+    # xList_1_1 = []
+    # yList = []
